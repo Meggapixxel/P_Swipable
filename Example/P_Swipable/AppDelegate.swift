7261e3106a69c587b4570612adaf3d86f1a79152
Meggapixxel <vzhydenko@competo.io>
2020-05-27 17:28:39 +0300

    missing file

--- Example/P_Swipable/AppDelegate.swift
+++ Example/P_Swipable/AppDelegate.swift
@@ -13,8 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        window.rootViewController = UINavigationController(rootViewController: SwipeableSelectionVC())
+        window.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
